8701a4d2da4d51d795293dd091063e5f13166cc2
lockhart3xgenz-svg <lockhart3xgenz@gmail.com>
2026-01-26 15:17:37 +0200

    Footer component added

--- src/App.js
+++ src/App.js
@@ -2,26 +2,26 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./Components/Navbar";
-// import Footer from "./Components/Footer"
+import Footer from "./Components/Footer";
 
-//import About from "Pages/About";
-//import Contact from "Pages/Contact";
-//import Home from "Pages/Home";
-//import Projects from "Pages/Projects"
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
+import Projects from "./Pages/Projects";
 
 function App() {
-  return(
+  return (
     <BrowserRouter>
-    <Navbar />
+      <Navbar />
 
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/About" element={<About />} />
-      <Route path="/Contact" element={<Contact />} />
-      <Route path="/Projects" element={<Projects />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
 
-    <Footer />
+      <Footer />
     </BrowserRouter>
   );
 }
